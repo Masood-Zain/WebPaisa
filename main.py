4158e3b43687e53e53d4e3b901a6c9cdaf5c2a36
Masood-Zain <113947256+Masood-Zain@users.noreply.github.com>
2022-11-09 13:36:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,34 @@ print ("email webpaisa@yopmail.com")
 print (" > to create/change username or password")
 print (" > to transfer money")
 print (" ")
-
 print ("Username")
 username = input()
+
 if username == "admin":
   print ("Password")
   password = input()
   if password == "password":
     print ("Total Balance: 1000 ETH")
+    
 if username == "anish":
+  print ("Password")
+  password = input()
+  if password == "password":
+    print ("Total Balance: 100 ETH")
+    
+if username == "dheera":
+  print ("Password")
+  password = input()
+  if password == "password":
+    print ("Total Balance: 100 ETH")
+
+if username == "prudh":
+  print ("Password")
+  password = input()
+  if password == "password":
+    print ("Total Balance: 100 ETH")
+
+if username == "shivansh":
   print ("Password")
   password = input()
   if password == "password":
